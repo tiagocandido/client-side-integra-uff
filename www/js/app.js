5593d33aed933fa5ebeaabcba4c160f01601fd82
Tiago Cândido <tiago.candido@bcc.ic.uff.br>
2015-06-22 12:15:05 -0300

    Corrige a navegação aninhada, itens que não term botão na barra de abas, devem renderizar na tab-dash.

--- www/js/app.js
+++ www/js/app.js
@@ -30,7 +30,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   $stateProvider
 
   // setup an abstract state for the tabs directive
-    .state('tab', {
+  .state('tab', {
     url: "/tab",
     abstract: true,
     templateUrl: "templates/tabs.html"
@@ -52,8 +52,8 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       url: '/chats',
       views: {
         'tab-chats': {
-          templateUrl: 'templates/tab-courses.html',
-          controller: 'CoursesCtrl'
+          templateUrl: 'templates/tab-chats.html',
+          controller: 'ChatsCtrl'
         }
       }
     })
@@ -80,7 +80,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   .state('tab.courses', {
     url: '/courses',
     views: {
-      'tab-courses': {
+      'tab-dash': {
         templateUrl: 'templates/tab-courses.html',
         controller: 'CoursesCtrl'
       }
