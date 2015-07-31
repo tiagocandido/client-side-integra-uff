43f210d319aa1d9ea5c284b768d98bc5b0cdd267
Tiago Cândido <tiago.candido@bcc.ic.uff.br>
2015-07-31 20:31:32 -0300

    - Adds a loading spinner while the app is Syncing data after login

--- www/js/controllers/login.ctrl.js
+++ www/js/controllers/login.ctrl.js
@@ -1,12 +1,22 @@
-function LoginCtrl($scope, $state, Authentication, Sync) {
-  $scope.credentials = {};
+function LoginCtrl($scope, $state, $ionicLoading, Authentication, Sync) {
   $scope.login = function(){
     Authentication.login($scope.credentials)
         .then(function(){
-          $scope.notice = "Sincronizando dados";
+          startSyncing();
           Sync.run().then(function(){
+            stopSyncing();
             $state.transitionTo('tab.dash');
           });
         });
+  };
+
+  var startSyncing = function(){
+    $ionicLoading.show({
+      templateUrl: 'templates/syncing.html'
+    });
+  };
+
+  var stopSyncing = function(){
+    $ionicLoading.hide();
   }
 }
