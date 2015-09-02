8874b4d68f53b8d1b27e0ea4de5397a24e5124e7
Igor Barbosa <igorbarbosa.p@gmail.com>
2015-09-02 17:17:06 -0300

    Fix file download directory

--- www/js/controllers/files.ctrl.js
+++ www/js/controllers/files.ctrl.js
@@ -1,19 +1,25 @@
-function FilesCtrl($scope, Files, $cordovaFileTransfer, $ionicPlatform) {
+function FilesCtrl($scope, Files, $cordovaFileTransfer, $ionicPlatform, Accounts, $timeout) {
   Files.all().then(function(files){
     $scope.files = files
   });
 
-  $scope.FileDownload = function (file) {
-      var filePath = [file.system, file.course_name, file.system_id, file.file_name].join("/")
-      // var targetPath = cordova.file.externalRootDirectory + filePath;
+  $scope.FileDownload = function(file){
+      var filePath = [file.system, file.course_name.replace('/','-'), file.system_id, file.file_name].join("/")
+      var targetPath = cordova.file.externalRootDirectory + '/integrauff/' + filePath;
+      var options = {}
 
-      $ionicPlatform.ready(function() {
-        $cordovaFileTransfer.download(file.download_url, filePath, {}, true).then(function (result) {
-            console.log('Success');
-        }, function (error) {
-            console.log('Error');
-        }, function (progress) {
-            // PROGRESS HANDLING GOES HERE
+      //TODO: generic header builder
+      Accounts.getToken(file.system).then(function(token){
+        options.headers = { "AUTHORIZATION": "Token token=" + token };
+
+        $ionicPlatform.ready(function() {
+          $cordovaFileTransfer.download(file.download_url, targetPath, options, true).then(function (result) {
+              console.log('Success');
+          }, function (error) {
+              console.log('Error');
+          }, function (progress) {
+
+          });
         });
       });
   };
