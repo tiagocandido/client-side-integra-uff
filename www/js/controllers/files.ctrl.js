0b4d163153b05779e731624fb078f1e8bdc4d7ce
Igor Barbosa <igorbarbosa.p@gmail.com>
2015-09-02 14:59:08 -0300

    Add files download feature
    * Added ngCordova
    * Added cordova file plugin
    * Added cordova fileTransfer plugin

--- www/js/controllers/files.ctrl.js
+++ www/js/controllers/files.ctrl.js
@@ -1,5 +1,21 @@
-function FilesCtrl($scope, Files) {
+function FilesCtrl($scope, Files, $cordovaFileTransfer, $ionicPlatform) {
   Files.all().then(function(files){
     $scope.files = files
   });
+
+  $scope.FileDownload = function (file) {
+      var filePath = [file.system, file.course_name, file.system_id, file.file_name].join("/")
+      // var targetPath = cordova.file.externalRootDirectory + filePath;
+
+      $ionicPlatform.ready(function() {
+        $cordovaFileTransfer.download(file.download_url, filePath, {}, true).then(function (result) {
+            console.log('Success');
+        }, function (error) {
+            console.log('Error');
+        }, function (progress) {
+            // PROGRESS HANDLING GOES HERE
+        });
+      });
+  };
+
 }
