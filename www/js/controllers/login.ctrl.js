75ab465919c872418f034ca0718537c1d29e39a1
Tiago Cândido <tiago.candido@bcc.ic.uff.br>
2015-09-09 18:11:23 -0300

    Add loading message to the authentication

--- www/js/controllers/login.ctrl.js
+++ www/js/controllers/login.ctrl.js
@@ -1,8 +1,12 @@
-function LoginCtrl($scope, $state, $ionicPopup, Authentication, Sync) {
+function LoginCtrl($scope, $state, $ionicPopup, $ionicLoading, Authentication, Sync) {
   $scope.credentials = {};
   $scope.login = function(){
+    $ionicLoading.show({
+        templateUrl: 'templates/authenticating.html'
+    });
     Authentication.login('conexao_uff', $scope.credentials)
         .then(function(){
+          $ionicLoading.hide();
           Sync.now().then(function(){
             $state.transitionTo('tab.dash');
           });
