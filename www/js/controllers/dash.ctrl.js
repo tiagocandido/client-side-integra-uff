39c7984059b9d13ae24219939e6c2d72fe22cb91
Tiago Cândido <tiago.candido@bcc.ic.uff.br>
2015-09-09 18:09:14 -0300

    Redirect to accounts in two steps
    - Redirect first to state settings then accounts, in order to keep state history
    - Add logging when syncing

--- www/js/controllers/dash.ctrl.js
+++ www/js/controllers/dash.ctrl.js
@@ -1,15 +1,19 @@
 function DashCtrl($scope, $ionicLoading, $state) {
     $scope.$on("NOT_AUTHENTICATED", function () {
-        $state.transitionTo('tab.accounts');
+        $state.go('tab.settings').then(function(){
+            $state.go('tab.accounts')
+        });
     });
 
     $scope.$on("SYNC_START", function () {
+        console.log("SYNC_START");
         $ionicLoading.show({
             templateUrl: 'templates/syncing.html'
         });
     });
 
     $scope.$on("SYNC_STOP", function () {
+        console.log("SYNC_STOP");
         $ionicLoading.hide();
     });
 }
