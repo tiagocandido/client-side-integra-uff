6354e59ddb1dfe634ac583060aa504e3ef8ee7bb
Igor Barbosa <igorbarbosa.p@gmail.com>
2015-09-16 18:53:52 -0300

    Refactor add event to calendar style

--- www/js/controllers/events.ctrl.js
+++ www/js/controllers/events.ctrl.js
@@ -1,4 +1,4 @@
-function EventsCtrl($scope, Events) {
+function EventsCtrl($scope, $cordovaCalendar, $ionicPopup, Events) {
   Events.all().then(function(events){
     var time_now = new Date().toISOString();
     var next_events = [];
@@ -15,4 +15,24 @@ function EventsCtrl($scope, Events) {
     $scope.next_events = next_events;
     $scope.previous_events = previous_events;
   });
+
+  $scope.addToCalendar = function(_event) {
+    $cordovaCalendar.createEvent({
+      title: _event.name,
+      notes: _event.info,
+      startDate: _event.starts,
+      endDate: _event.ends
+    }).then(function (result) {
+      console.log("Deu bom");
+      $ionicPopup.alert({
+        title: 'Evento adicionado',
+        template: 'O evento ' + _event.name + ' foi adicionado ao calendário com sucesso.'
+      });
+    }, function (err) {
+      $ionicPopup.alert({
+        title: 'Falha',
+        template: 'Erro: ' + err
+      });
+    });
+  };
 }
